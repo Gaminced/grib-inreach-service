1fcb215c28d1fe9b1edada89f76fc4f29610ecb8
Gaminced <garminced@gmail.com>
2025-12-19 14:54:55 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,5 +4,15 @@ set -o errexit
 
 pip install --upgrade pip
 pip install -r requirements.txt
+
+# Installer uniquement le browser sans les dépendances système
+PLAYWRIGHT_SKIP_BROWSER_DOWNLOAD=0 playwright install chromium
+
+# Les dépendances chromium seront installées automatiquement par Render#!/usr/bin/env bash
+# exit on error
+set -o errexit
+
+pip install --upgrade pip
+pip install -r requirements.txt
 playwright install chromium
 playwright install-deps chromium --no-shell
