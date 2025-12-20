bef7af2c4f55be517e35a9325938a70a58438778
Gaminced <garminced@gmail.com>
2025-12-19 20:20:00 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 set -o errexit
 
+export PLAYWRIGHT_BROWSERS_PATH=/opt/render/project/src/.browsers
+
 pip install --upgrade pip
 pip install -r requirements.txt
+playwright install chromium
 
-# Installer Chromium avec le bon path
-PLAYWRIGHT_BROWSERS_PATH=$PWD/.browsers playwright install chromium
+echo "Chromium installed at: $PLAYWRIGHT_BROWSERS_PATH"
+ls -la $PLAYWRIGHT_BROWSERS_PATH
